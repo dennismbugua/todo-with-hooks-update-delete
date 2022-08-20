f52adb510c14290a2ee85e954f53733f298b2793
Dennis Mbugua <dennis@starlaunch.org>
2022-08-20 13:40:17 +0300

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,104 @@
+import React, { useState } from "react";
+import ReactDOM from "react-dom";
+
+import "./styles.css";
+
+function App() {
+  const [todos, setTodos] = useState([
+    { name: "Do pushups", status: true, id: 1 },
+    { name: "Do situps", status: false, id: 2 }
+  ]);
+
+  const [value, setValue] = useState("");
+
+  const onValueChange = ({ target: { value } }) => {
+    setValue(value);
+  };
+
+  const addTodo = () => {
+    if (value !== "") {
+      setTodos([
+        ...todos,
+        { name: value, status: false, id: Date.now() + Math.random() }
+      ]);
+      setValue("");
+    }
+  };
+
+  const handleKeyPress = ({ key }) => {
+    if (key === "Enter") {
+      addTodo();
+    }
+  };
+
+  const handleCheckboxChange = id => {
+    setTodos(
+      todos.map(todo => {
+        if (todo.id === id) return { ...todo, status: !todo.status };
+        return todo;
+      })
+    );
+  };
+
+  const deleteTodo = id => {
+    setTodos(todos.filter(todo => todo.id !== id));
+  };
+
+  return (
+    <div className="container">
+      <p>
+        <label>Add Item</label>
+        <input
+          id="new-task"
+          type="text"
+          value={value}
+          name="todoField"
+          onKeyDown={handleKeyPress}
+          onChange={onValueChange}
+        />
+        <button onClick={addTodo}>Add</button>
+      </p>
+
+      <h3>Todo</h3>
+      <ul id="incomplete-tasks">
+        {todos
+          .filter(todo => !todo.status)
+          .map(todo => (
+            <li key={todo.id}>
+              <input
+                type="checkbox"
+                checked={todo.status}
+                onChange={() => handleCheckboxChange(todo.id)}
+              />
+              <label>{todo.name}</label>
+              <button className="delete" onClick={() => deleteTodo(todo.id)}>
+                Delete
+              </button>
+            </li>
+          ))}
+      </ul>
+
+      <h3>Completed</h3>
+      <ul id="completed-tasks">
+        {todos
+          .filter(todo => todo.status)
+          .map(todo => (
+            <li key={todo.id}>
+              <input
+                type="checkbox"
+                checked={todo.status}
+                onChange={() => handleCheckboxChange(todo.id)}
+              />
+              <label>{todo.name}</label>
+              <button className="delete" onClick={() => deleteTodo(todo.id)}>
+                Delete
+              </button>
+            </li>
+          ))}
+      </ul>
+    </div>
+  );
+}
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(<App />, rootElement);
